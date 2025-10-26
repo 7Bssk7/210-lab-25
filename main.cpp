646bffcf555edfeb347cb4c4f923f4abfd1c82c3
7Bssk7 <arhip.fin3@gmail.com>
2025-10-26 12:37:25 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -21,53 +21,53 @@ int main() {
 
     }
 
-    auto start = high_resolution_clock::now();
+    auto list_startR = high_resolution_clock::now();
     while(getline(inputFile,code)){
         list.push_back(code);
     }
-    auto end = high_resolution_clock::now();
-    auto list_duration = duration_cast<microseconds>(end - start);
+    auto list_endR = high_resolution_clock::now();
+    auto list_durationR = duration_cast<microseconds>(list_startR - list_endR);
 
-    cout << "List read: " << list_duration.count() << " microseconds" << endl;
+    cout << "List read: " << list_durationR.count() << " microseconds" << endl;
 
-    start = high_resolution_clock::now();
+    auto list_startS = high_resolution_clock::now();
     list.sort();
-    end = high_resolution_clock::now();
-    list_duration = duration_cast<microseconds>(end - start);
-    cout << "Vector sort: " << list_duration.count() << " microseconds" << endl;
+    auto list_endS = high_resolution_clock::now();
+    auto list_durationS = duration_cast<microseconds>(list_endS - list_startS);
+    cout << "List sort: " << list_durationS.count() << " microseconds" << endl;
 
     // This code moves the pointer inside inputFile back to the beginning of the file so we can read it again.
     inputFile.clear();
     inputFile.seekg(0, ios::beg);
 
-    start = high_resolution_clock::now();
+    auto set_startR = high_resolution_clock::now();
     while(getline(inputFile, code)){
         set.insert(code);
     }
-    end = high_resolution_clock::now();
-    auto set_duration= duration_cast<microseconds>(end - start);
+    auto set_endR = high_resolution_clock::now();
+    auto set_durationR = duration_cast<microseconds>(set_endR - set_startR);
 
-    cout << "Set read: " << set_duration.count() << " microseconds" << endl;
+    cout << "Set read: " << set_durationR.count() << " microseconds" << endl;
 
     cout << "Set sort: -1 microseconds" << endl;
 
     inputFile.clear();
     inputFile.seekg(0, ios::beg);
 
-    start = high_resolution_clock::now();
+    auto vector_startR = high_resolution_clock::now();
     while(getline(inputFile, code)){
         vector.push_back(code);
     }
-    end = high_resolution_clock::now();
-    auto vector_duration = duration_cast<microseconds>(end - start);
+    auto vector_endR = high_resolution_clock::now();
+    auto vector_duration = duration_cast<microseconds>(vector_endR - vector_startR);
 
     cout << "Vector read: " << vector_duration.count() << " microseconds" << endl;
 
-    start = high_resolution_clock::now();
+    auto vector_startS = high_resolution_clock::now();
     sort(vector.begin(), vector.end());
-    end = high_resolution_clock::now();
-    vector_duration = duration_cast<microseconds>(end - start);
-    cout << "Vector sort: " << vector_duration.count() << " microseconds" << endl;
+    auto vector_endS = high_resolution_clock::now();
+    auto vector_durationS = duration_cast<microseconds>(vector_endS - vector_startS);
+    cout << "Vector sort: " << vector_durationS.count() << " microseconds" << endl;
     
 
 
